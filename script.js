671f137e3535dba1f0d6d83b220e4047eca76b75
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-07-16 18:44:04 +0530

    Created a portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typed", {
+  strings: ["Software Developer", "Backend Developer", "Frontend Developer","Java Developer", "Full Stack Developer", "Software Development Engineer"],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true,
+});
